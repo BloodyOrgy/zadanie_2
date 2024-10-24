5cc860fdce10c61773010b22b6ee42e498634125
Satana <rad_devil@inbox.ru>
2024-10-24 20:41:35 +0400

    ReformatCode

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,11 @@ public class Main {
 
         int addend = 1100;
 
-             int bonus = addend > 1000 ? addend / 100 : 0;
+        int bonus = addend > 1000 ? addend / 100 : 0;
 
         int finalBalance = initialAccount + addend + bonus;
-        System.out.println("Бонус: "+ bonus);
+        System.out.println("Бонус: " + bonus);
         System.out.println("Итоговый счет:" + finalBalance);
-
 
 
     }
